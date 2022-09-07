431d60be7a4fefb153fbaf948c5c2cab0f8c4ed9
vardhan-saniko <vrsaniko@asu.edu>
2022-09-07 16:27:19 -0700

    updated Authentication part

--- CitesphereConnector.py
+++ CitesphereConnector.py
@@ -6,20 +6,26 @@ CITESPHERE_API = "https://citesphere.instance.org/api/v1"
 
 
 class CitesphereConnector:
-    def __init__(self, api_key, end_point=None):
-        self.api_key = api_key
+    def __init__(self, client_id, client_secret, end_point=None):
+        self.api_key = self.get_api_key(client_id, client_secret)
         self.end_point = end_point
+
+    def get_api_key(self, client_id, client_secret):
+        url = CITESPHERE_API + "/oauth/token?client_id={}&client_secret={}&grant_type=authorization_code".format(client_id, client_secret)
+        response = urllib2.urlopen(urllib2.Request(url, headers={}))
+        data = json.load(response)
+
+        return data["access_token"]
 
     def execute_command(self, url):
         try:
-            headers = {'Authorization': 'access_token {}'.format(self.api_key)}
+            headers = {'Authorization': 'Bearer {}'.format(self.api_key)}
             response = urllib2.urlopen(urllib2.Request(url, headers=headers))
             data = json.load(response)
 
             return data
         except Exception as exc:
             return {"error_message": str(exc)}
-
 
     def get_user(self):
         url = CITESPHERE_API + "/user"
@@ -28,7 +34,6 @@ class CitesphereConnector:
     def check_test(self):
         url = CITESPHERE_API + "/test"
         return self.execute_command(url)
-
 
     # Common method to get data for all endpoints
     def get_data(self, end_point):
