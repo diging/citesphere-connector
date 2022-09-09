77ccd9bb03db8d34bc8becb1048bf447c318e230
vardhan-saniko <vrsaniko@asu.edu>
2022-09-09 16:23:54 -0700

    Updated all end points of Citesphere

--- CitesphereConnector.py
+++ CitesphereConnector.py
@@ -2,20 +2,52 @@
 import urllib2
 import json
 
-CITESPHERE_API = "https://citesphere.instance.org/api/v1"
+# CITESPHERE_API = "https://citesphere.instance.org/api/v1"
+CITESPHERE_API = "https://diging-dev.asu.edu/citesphere-review/api"
 
 
 class CitesphereConnector:
-    def __init__(self, client_id, client_secret, end_point=None):
-        self.api_key = self.get_api_key(client_id, client_secret)
-        self.end_point = end_point
+    def __init__(self, **kwargs):
+        self.client_id = kwargs.get('client_id')
+        self.client_secret = kwargs.get('client_secret')
+        self.grant_type = kwargs.get('grant_type')
+        self.redirect_uri = kwargs.get('redirect_uri')
+        self.authorization_code = kwargs.get('authorization_code')
+        self.api_key = None
+        self.access_token = kwargs.get('access_token')
+        self.validate()
 
-    def get_api_key(self, client_id, client_secret):
-        url = CITESPHERE_API + "/oauth/token?client_id={}&client_secret={}&grant_type=authorization_code".format(client_id, client_secret)
-        response = urllib2.urlopen(urllib2.Request(url, headers={}))
-        data = json.load(response)
+    def validate(self):
+        if self.access_token:
+            response = self.check_test()
+            if response.get("error_message"):
+                raise Exception("Access Token is not valid: {}".format(response["error_message"]))
+            else:
+                return
+        if not self.client_id:
+            raise Exception("Client Id is required")
 
-        return data["access_token"]
+        if not self.client_secret:
+            raise Exception("Client Secret is required")
+
+        if not self.grant_type:
+            raise Exception("Grant Type is required")
+
+        self.get_api_key()
+
+
+    def get_api_key(self):
+        if self.grant_type == "client_credentials":
+            url = CITESPHERE_API + "/oauth/token?client_id={}&client_secret={}&grant_type=authorization_code".format(self.client_id, self.client_secret)
+            response = urllib2.urlopen(urllib2.Request(url, headers={}))
+            data = json.load(response)
+        else:
+            raise Exception("Invalid Grant Type")
+
+        self.api_key = data["access_token"]
+
+        return None
+
 
     def execute_command(self, url):
         try:
@@ -27,15 +59,48 @@ class CitesphereConnector:
         except Exception as exc:
             return {"error_message": str(exc)}
 
+
     def get_user(self):
-        url = CITESPHERE_API + "/user"
+        url = CITESPHERE_API + "/v1/user"
         return self.execute_command(url)
+
 
     def check_test(self):
-        url = CITESPHERE_API + "/test"
+        url = CITESPHERE_API + "/v1/test"
         return self.execute_command(url)
 
-    # Common method to get data for all endpoints
-    def get_data(self, end_point):
-        url = CITESPHERE_API + end_point
+
+    # Common method to get data based on endpoint
+    def get_data_by_endpoint(self, end_point):
+        url = CITESPHERE_API + "/v1" + end_point
+        return self.execute_command(url)
+
+
+    def get_groups(self):
+        url = CITESPHERE_API + "/v1/groups"
+        return self.execute_command(url)
+
+
+    def get_group_info(self, group_id):
+        url = CITESPHERE_API + "/v1/groups/{}".format(group_id)
+        return self.execute_command(url)
+
+
+    def get_group_items(self, zotero_group_id):
+        url = CITESPHERE_API + "/v1/groups/{}/items".format(zotero_group_id)
+        return self.execute_command(url)
+
+
+    def get_collections(self, zotero_group_id):
+        url = CITESPHERE_API + "/v1/groups/{}/collections".format(zotero_group_id)
+        return self.execute_command(url)
+
+
+    def get_collection_items(self, zotero_group_id, collection_id):
+        url = CITESPHERE_API + "/v1/groups/{}/collections/{}/items".format(zotero_group_id, collection_id)
+        return self.execute_command(url)
+
+
+    def get_item_info(self, zotero_group_id, item_id):
+        url = CITESPHERE_API + "/v1/groups/{}/items/{}".format(zotero_group_id, item_id)
         return self.execute_command(url)
