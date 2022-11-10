f7f579b36ead60f165b9dceeea8dcf59fea765a0
vardhan-saniko <vrsaniko@asu.edu>
2022-11-10 16:26:27 -0700

    [CICO-1] addressed comments

--- CitesphereConnector.py
+++ CitesphereConnector.py
@@ -1,106 +1,91 @@
 
-import urllib2
+import urllib.request as urllib2
 import json
 
-# CITESPHERE_API = "https://citesphere.instance.org/api/v1"
-CITESPHERE_API = "https://diging-dev.asu.edu/citesphere-review/api"
+
+class BearerAccessTokenAuthorization:
+    def __init__(self, access_token):
+        self.headers = {'Authorization': 'Bearer {}'.format(access_token)}
+
+
+class OAuth2ClientAuthorization:
+    def __init__(self, api, kwargs):
+        self.api = api
+        self.client_id = kwargs.get('client_id')
+        self.client_secret = kwargs.get('client_secret')
+        self.headers = BearerAccessTokenAuthorization(self.get_access_token()).headers
+
+    def get_access_token(self):
+        url = self.api + "/oauth/token?client_id={}&client_secret={}&grant_type=client_credentials".format(
+            self.client_id, self.client_secret)
+        response = urllib2.urlopen(urllib2.Request(url, headers={}))
+        data = json.load(response)
+
+        return data['access_token']
+
+
+def validate(kwargs, api):
+    if not api:
+        raise Exception("API is required")
+
+    if not kwargs.get('access_token') and not (kwargs.get('grant_type') and kwargs.get('client_id') and kwargs.get('client_secret')):
+        raise Exception("Insufficient authentication details. Either Access token is required or client id, secret, grant type are required")
 
 
 class CitesphereConnector:
-    def __init__(self, **kwargs):
-        self.client_id = kwargs.get('client_id')
-        self.client_secret = kwargs.get('client_secret')
-        self.grant_type = kwargs.get('grant_type')
-        self.redirect_uri = kwargs.get('redirect_uri')
-        self.authorization_code = kwargs.get('authorization_code')
-        self.api_key = None
-        self.access_token = kwargs.get('access_token')
-        self.validate()
+    def __init__(self, api, **kwargs):
+        self.api = api
+        validate(kwargs, api)
+        self.oauth_object = self.get_oauth_object(kwargs)
 
-    def validate(self):
-        if self.access_token:
-            response = self.check_test()
-            if response.get("error_message"):
-                raise Exception("Access Token is not valid: {}".format(response["error_message"]))
-            else:
-                return
-        if not self.client_id:
-            raise Exception("Client Id is required")
-
-        if not self.client_secret:
-            raise Exception("Client Secret is required")
-
-        if not self.grant_type:
-            raise Exception("Grant Type is required")
-
-        self.get_api_key()
-
-
-    def get_api_key(self):
-        if self.grant_type == "client_credentials":
-            url = CITESPHERE_API + "/oauth/token?client_id={}&client_secret={}&grant_type=authorization_code".format(self.client_id, self.client_secret)
-            response = urllib2.urlopen(urllib2.Request(url, headers={}))
-            data = json.load(response)
+    def get_oauth_object(self, kwargs):
+        if kwargs.get('grant_type') == "client_credentials":
+            return OAuth2ClientAuthorization(self.api, kwargs)
         else:
-            raise Exception("Invalid Grant Type")
-
-        self.api_key = data["access_token"]
-
-        return None
-
+            return BearerAccessTokenAuthorization(kwargs.get('access_token'))
 
     def execute_command(self, url):
         try:
-            headers = {'Authorization': 'Bearer {}'.format(self.api_key)}
-            response = urllib2.urlopen(urllib2.Request(url, headers=headers))
+            response = urllib2.urlopen(urllib2.Request(url, headers=self.oauth_object.headers))
             data = json.load(response)
 
             return data
         except Exception as exc:
             return {"error_message": str(exc)}
 
-
     def get_user(self):
-        url = CITESPHERE_API + "/v1/user"
+        url = self.api + "/v1/user"
         return self.execute_command(url)
-
 
     def check_test(self):
-        url = CITESPHERE_API + "/v1/test"
+        url = self.api + "/v1/test"
         return self.execute_command(url)
-
 
     # Common method to get data based on endpoint
     def get_data_by_endpoint(self, end_point):
-        url = CITESPHERE_API + "/v1" + end_point
+        url = self.api + "/v1" + end_point
         return self.execute_command(url)
-
 
     def get_groups(self):
-        url = CITESPHERE_API + "/v1/groups"
+        url = self.api + "/v1/groups"
         return self.execute_command(url)
-
 
     def get_group_info(self, group_id):
-        url = CITESPHERE_API + "/v1/groups/{}".format(group_id)
+        url = self.api + "/v1/groups/{}".format(group_id)
         return self.execute_command(url)
-
 
     def get_group_items(self, zotero_group_id):
-        url = CITESPHERE_API + "/v1/groups/{}/items".format(zotero_group_id)
+        url = self.api + "/v1/groups/{}/items".format(zotero_group_id)
         return self.execute_command(url)
-
 
     def get_collections(self, zotero_group_id):
-        url = CITESPHERE_API + "/v1/groups/{}/collections".format(zotero_group_id)
+        url = self.api + "/v1/groups/{}/collections".format(zotero_group_id)
         return self.execute_command(url)
-
 
     def get_collection_items(self, zotero_group_id, collection_id):
-        url = CITESPHERE_API + "/v1/groups/{}/collections/{}/items".format(zotero_group_id, collection_id)
+        url = self.api + "/v1/groups/{}/collections/{}/items".format(zotero_group_id, collection_id)
         return self.execute_command(url)
 
-
     def get_item_info(self, zotero_group_id, item_id):
-        url = CITESPHERE_API + "/v1/groups/{}/items/{}".format(zotero_group_id, item_id)
+        url = self.api + "/v1/groups/{}/items/{}".format(zotero_group_id, item_id)
         return self.execute_command(url)
