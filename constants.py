19a72e0943463a9647f7a6cee47271475271674f
Julian Ophals <jophals@asu.edu>
2025-01-03 14:18:11 -0700

    [story/CICO-15] implemented PR fixes: removed print output, modified urls/constants

--- constants.py
+++ constants.py
@@ -1,3 +1,1 @@
-CITESPHERE_API_URL = "https://diging-dev.asu.edu/citesphere-review/api"
-MAX_SIZE = 50
-GILES_URL = f"https://diging.asu.edu/geco-giles-staging/api/v2/resources/files/"
+GILES_ROOT = "https://diging-dev.asu.edu/citesphere-review/api"
