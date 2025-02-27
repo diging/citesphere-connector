b6f2a6ce58434c3070b8e379079f87eb93c7ac82
Julian Ophals <113470908+jophals@users.noreply.github.com>
2025-02-27 14:40:02 -0700

    Story/cico 13 (#11)
    * [story/CICO-7] added methods for remaining existing endpoints and cleaned up syntax
    
    * [CICO-13] added requirements.txt file
    
    * [story/CICO-13] removed useless methods to address PR changes
    
    * [CICO-6] added pre-commit, added ruff Github action, added ruff pre-commit, started working on fixing add_item method, updated requirements.txt, started working on PyPi structure
    
    * [CICO-6] Added toml for PyPi preparation, updated README, fixed add_item endpoint
    
    * [CICO-6] added setup.py file, added __init__.py files, updated README, updated .toml file, added some tests
    
    * [story/CICO-13] implemented PR fixes
    
    * [story/CICO-13] Deleted src/__pycache__ directory
    
    * [story/CICO-13] added pre-commit install step to README
    
    * [story/CICO-13] renamed github-actions yaml

---
+++ src/CitesphereConnector.py
@@ -0,0 +1,132 @@
+import urllib.request as urllib2
+import json
+import base64
+import requests
+import os
+
+
+class CitesphereConnector:
+    def __init__(self, api, auth_token_object):
+        self.api = api
+        self.auth_token_object = auth_token_object
+        self.validate()
+        self.handle_api_params()
+
+    def validate(self):
+        if not hasattr(self.auth_token_object, "authType"):
+            raise AttributeError("Missing authType attribute")
+
+        if not hasattr(self.auth_token_object, "headers"):
+            raise AttributeError("Missing headers attribute")
+
+        if not hasattr(self.auth_token_object, "access_token"):
+            if not hasattr(self.auth_token_object, "username") and not hasattr(
+                self.auth_token_object, "password"
+            ):
+                raise AttributeError(
+                    "Either username and password or access_token should be present"
+                )
+
+        if (
+            not self.auth_token_object.authType == "oauth"
+            and not self.auth_token_object.authType == "basic"
+        ):
+            raise Exception("authType should be either oauth or basic")
+
+    def handle_api_params(self):
+        if self.auth_token_object.authType == "oauth":
+            self.auth_token_object.headers = {
+                "Authorization": f"Bearer {self.auth_token_object.access_token}",
+            }
+        elif self.auth_token_object.authType == "basic":
+            auth_str = (
+                f"{self.auth_token_object.username}:{self.auth_token_object.password}"
+            )
+            auth_b64 = base64.b64encode(auth_str.encode("ascii"))
+            self.auth_token_object.headers = {"Authorization": f"Basic {auth_b64}"}
+
+    def execute_command(self, url):
+        try:
+            response = urllib2.urlopen(
+                urllib2.Request(url, headers=self.auth_token_object.headers)
+            )
+            data = json.load(response)
+
+            return data
+        except Exception as exc:
+            return {"error_message": str(exc)}
+
+    def execute_post_request(self, url, data, files):
+        try:
+            response = requests.post(
+                url, headers=self.auth_token_object.headers, data=data, files=files
+            )
+            print(response.status_code)
+            # Uncomment for debugging response from Citesphere
+            # print(response.text)
+            return response
+        except Exception as exc:
+            return {"error_message": str(exc)}
+
+    def get_user(self):
+        url = f"{self.api}/v1/user"
+        return self.execute_command(url)
+
+    def check_test(self):
+        url = f"{self.api}/v1/test"
+        return self.execute_command(url)
+
+    def check_access(self, document_id):
+        url = f"{self.api}/files/giles/{document_id}/access/check"
+        return self.execute_command(url)
+
+    # Common method to get data based on endpoint
+    def get_data_by_endpoint(self, end_point):
+        url = f"{self.api}/v1{end_point}"
+        return self.execute_command(url)
+
+    def get_groups(self):
+        url = f"{self.api}/v1/groups"
+        return self.execute_command(url)
+
+    def get_group_info(self, group_id):
+        url = f"{self.api}/v1/groups/{group_id}"
+        return self.execute_command(url)
+
+    def get_group_items(self, zotero_group_id):
+        url = f"{self.api}/v1/groups/{zotero_group_id}/items"
+        return self.execute_command(url)
+
+    def get_collections(self, zotero_group_id):
+        url = f"{self.api}/v1/groups/{zotero_group_id}/collections"
+        return self.execute_command(url)
+
+    def get_collection_items(self, zotero_group_id, collection_id, page_number=0):
+        url = (
+            f"{self.api}/v1/groups/{zotero_group_id}/collections/{collection_id}/items"
+        )
+        if page_number:
+            url = f"{url}?&page={page_number}"
+        return self.execute_command(url)
+
+    def get_item_info(self, zotero_group_id, item_id):
+        url = f"{self.api}/v1/groups/{zotero_group_id}/items/{item_id}"
+        return self.execute_command(url)
+
+    def get_collections_by_collection_id(self, zotero_group_id, collection_id):
+        url = f"{self.api}/groups/{zotero_group_id}/collections/{collection_id}/collections"
+        return self.execute_command(url)
+
+    def add_item(self, group_id, data, file_path):
+        try:
+            with open(file_path, "rb") as file_obj:
+                files = [(os.path.basename(file_path), file_obj)]
+                request_files = [
+                    ("files", (name, file, "application/pdf")) for name, file in files
+                ]
+                url = f"{self.api}/v1/groups/{group_id}/items/create"
+
+                return self.execute_post_request(url, data, request_files)
+        except Exception as e:
+            print(f"[ERROR] -------- Error during API request with {file_path}: {e}")
+            return "Error loading/reading file"
