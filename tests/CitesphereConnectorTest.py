74456207824b29c054adf1c16d41816943f560a5
Julian Ophals <jophals@asu.edu>
2025-03-13 16:31:56 -0700

    [story/CICO-15] fixed tests file

--- tests/CitesphereConnectorTest.py
+++ tests/CitesphereConnectorTest.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch, mock_open
 from src.CitesphereConnector import CitesphereConnector
-from src.authentication import AuthObject
+from AuthObject import AuthObject
 
 
 class EmptyObject:
