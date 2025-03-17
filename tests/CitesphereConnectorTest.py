e6b020c1b7d39e1c3eac74390e4c2fa481474d59
Julian Ophals <jophals@asu.edu>
2025-03-17 12:44:24 -0700

    [story/CICO-15] fixed auth_type property in test file

--- tests/CitesphereConnectorTest.py
+++ tests/CitesphereConnectorTest.py
@@ -11,7 +11,7 @@ class EmptyObject:
 class CitesphereConnectorTest(unittest.TestCase):
     def setUp(self):
         self.auth_object = AuthObject()
-        self.auth_object.authType = "oauth"
+        self.auth_object.auth_type = "oauth"
 
     def tearDown(self):
         del self.auth_object
